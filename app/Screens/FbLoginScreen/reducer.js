ff5f4173f5dbfdc986a4ba4355b1adde97c8286e
MaxVinohradov <vinogradov.max97@gmail.com>
2019-05-05 18:28:51 +0300

    refactor: migrate redux storage to immutable.js

--- app/Screens/FbLoginScreen/reducer.js
+++ app/Screens/FbLoginScreen/reducer.js
@@ -1,41 +1,37 @@
+import { Map } from 'immutable'
+
 import {
   FB_GET_PROFILE_DATA,
   FB_GET_PROFILE_DATA_SUCCESS,
-  FB_GET_PROFILE_IMG,
-  FB_GET_PROFILE_IMG_SUCCESS,
 } from '../../constants'
 
-const initialState = {
-  token: {
+const initialState = Map({
+  token: Map({
     expirationTime: 0,
     lastRefreshTime: 0,
     accessTokenSource: '',
     accessToken: '',
-  },
+  }),
   userId: '',
   email: '',
   first_name: '',
   last_name: '',
   name: '',
   gender: '',
-  profileImage: {
+  profileImage: Map({
     height: 0,
     width: 0,
     url: 0,
-  },
+  }),
   isFetching: false,
-}
+})
 
 export default function signInReducer (state = initialState, action) {
   switch (action.type) {
     case FB_GET_PROFILE_DATA:
-      return { ...state, isFetching: true }
+      return state.merge({ isFetching: true })
     case FB_GET_PROFILE_DATA_SUCCESS:
-      return { ...state, isFetching: false, ...action.payload }
-    case FB_GET_PROFILE_IMG:
-      return { ...state, isFetching: true }
-    case FB_GET_PROFILE_IMG_SUCCESS:
-      return { ...state, isFetching: false, ...action.payload }
+      return state.merge({ isFetching: false, ...action.payload })
     default:
       return state
   }
